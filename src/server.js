53fda0a50b619b30d3d631781f82269951e841ad
Nicole Faustino <nicoleadifaustino@gmail.com>
2025-03-17 13:55:52 +0800

    Update server.js

--- src/server.js
+++ src/server.js
@@ -45,13 +45,13 @@ admin.initializeApp({
 
 // Firebase configuration for the web app
 const firebaseConfig = {
-  apiKey: "AIzaSyC43LDkmXcXYtqqP26B-TM4ZgVZa8Q9fOM",
-  authDomain: "lost-n-found-unified-database.firebaseapp.com",
-  projectId: "lost-n-found-unified-database",
-  storageBucket: "lost-n-found-unified-database.firebasestorage.app",
-  messagingSenderId: "1003382149020",
-  appId: "1:1003382149020:web:e507cc98cfa1fc01c54259",
-  measurementId: "G-ZYFGKRMVV8",
+  apiKey: process.env.FIREBASE_API_KEY,
+  authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.FIREBASE_PROJECT_ID,
+  storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.FIREBASE_APP_ID,
+  measurementId: process.env.FIREBASE_MEASUREMENT_ID,
 };
 
 // Initialize Firebase app and Firestore database
