42f95c229f567ad14460e8af8f3a3250ecbc79cd
Nicole Faustino <nicoleadifaustino@gmail.com>
2025-03-21 22:25:38 +0800

    Updated Protected Routes

--- src/ProtectedRoute.jsx
+++ src/ProtectedRoute.jsx
@@ -1,6 +1,6 @@
 import { Navigate } from "react-router-dom";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../firebase"; // Make sure this path is correct
+import { auth } from "./firebase"; // Make sure this path is correct
 
 const ProtectedRoute = ({ children }) => {
   const [user, loading] = useAuthState(auth); // Get the authenticated user
--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import ReactDOM from "react-dom/client";
 import "./index.css";
-import ProtectedRoute from "./components/ProtectedRoute.jsx";
+import ProtectedRoute from "./ProtectedRoute.jsx";
 
 // Pages & Components
 import Home from "./Home";
